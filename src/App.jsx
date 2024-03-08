74162f2cd46f93eb0b1a476724de4c5737cabfb5
Aakash <aakashchaturvedi7@gmail.com>
2024-03-08 11:44:27 +0100

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./component/header/Header.jsx";
 import css from "./styles/app.module.scss"
+import React from "react";
 
 const App = () => {
   return <div className={'bg-primary ${css.container}'}>
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import css from './Header.module.scss'
+import {BiPhoneCall} from "react-icons/bi";
 
 const Header = () => {
-    return <div> Header </div>
+    return (
+        <div className={`paddings ${css.wrapper}`}>
+
+            <div className={`flexCenter innerWidth ${css.container}`}>
+                <div className={css.name}>Aakash</div>
+
+                <ul className={`flexCenter ${css.menu}`}>
+                <li><a href="">Services</a></li>
+                <li><a href="">Experience</a></li>
+                <li><a href="">Portfolio</a></li>
+                <li><a href="">Testimonials</a></li>
+                <li className={`flexCenter ${css.phone}`}>
+                    <p>+12345676788</p>
+                    <BiPhoneCall size={"40px"}>
+                    </BiPhoneCall>
+                </li>
+            </ul>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Header
